602d236165c34524b2d1fc38b9310327c1b1bd66
RajuGoudAdula <rajadula698@gmail.com>
2025-05-05 11:41:29 +0530

    testing

--- src/components/common/NotificationPanel.js
+++ src/components/common/NotificationPanel.js
@@ -55,10 +55,11 @@ const NotificationPanel = () => {
     registerUserForNotifications(user.id, user.role);
 
     const handler = async (notification) => {
-      const res = await dispatch(addNotification(notification));
-      if(notification?.refModel === "Canteen"){
-        dispatch(fetchCanteenStatus(user?.canteen?._id));
-      }
+       dispatch(addNotification(notification)).then(()=>{
+         if(notification?.refModel === "Canteen"){
+           dispatch(fetchCanteenStatus(user?.canteen?._id));
+         }
+       })
     };
 
     listenForNotifications(handler);
